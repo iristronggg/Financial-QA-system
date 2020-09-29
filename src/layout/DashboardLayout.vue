525755053672ea0fcaac3e8554bc0a91da8d90d2
iristronggg <b06705033@ntu.edu.tw>
2020-09-29 14:32:36 +0800

    all

---
+++ src/layout/DashboardLayout.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <mobile-menu slot="content"></mobile-menu>
+      <sidebar-link to="/admin/about">
+        <i class="nc-icon nc-app"></i>
+        <p>關於我們</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/user">
+        <i class="nc-icon nc-bulb-63"></i>
+        <p>提出查詢</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/table-list">
+        <i class="nc-icon nc-notes"></i>
+        <p>歷史詢問</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/typography">
+        <i class="nc-icon nc-light-3"></i>
+        <p>大家都在問</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/overview">
+        <i class="nc-icon nc-app"></i>
+        <p>統計圖們</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/icons">
+        <i class="nc-icon nc-atom"></i>
+        <p>Icons</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/maps">
+        <i class="nc-icon nc-pin-3"></i>
+        <p>Maps</p>
+      </sidebar-link>
+      <sidebar-link to="/admin/notifications">
+        <i class="nc-icon nc-bell-55"></i>
+        <p>Notifications</p>
+      </sidebar-link>
+
+      <template slot="bottom-links">
+        <sidebar-link class="active"
+                      to="/admin/upgrade">
+          <i class="nc-icon nc-alien-33"></i>
+          <p>Upgrade to PRO</p>
+        </sidebar-link>
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+
+</style>
+<script>
+  import TopNavbar from './TopNavbar.vue'
+  import ContentFooter from './ContentFooter.vue'
+  import DashboardContent from './Content.vue'
+  import MobileMenu from './MobileMenu.vue'
+  export default {
+    components: {
+      TopNavbar,
+      ContentFooter,
+      DashboardContent,
+      MobileMenu
+    },
+    methods: {
+      toggleSidebar () {
+        if (this.$sidebar.showSidebar) {
+          this.$sidebar.displaySidebar(false)
+        }
+      }
+    }
+  }
+
+</script>
